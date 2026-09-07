30f0cfbe1e193b9172071d9e51c44cf407ef1f6f
curtiscoombes <curtiswcoombes@gmail.com>
2026-09-07 17:23:37 +0100

    - Created separate datasets for ruminants and poultry as the weight measurement units are different - Filtered so it only goes up to 2026 - Filtered out unrealistically small values - Changed the theme - Added scales

--- R/meat_analysis.R
+++ R/meat_analysis.R
@@ -13,3 +13,38 @@ meat <- read_csv(here("Data", "Raw", "meat_consumption.csv"))
 # Initialise git repository
 use_git()
 use_github()
+
+# View the data
+head(meat)
+str(meat)
+
+# Clean the data's names using Janitor
+meat <- meat|>
+  clean_names()
+
+# Understanding the individual countries, as some may overlap
+meat|>
+  distinct(location)|>
+  print(n = Inf)
+
+# The previous step showed overlap (the world total, OECD, EU27, BRICS)
+# We need to filter those out
+meat|>
+  filter(!location %in% c("WLD", "OECD", "EU27", "BRICS"))|>
+  group_by(location)|>
+  summarise(mean = mean(value))|>
+  arrange(desc(mean))
+
+# Let's filter the data to only Great Britain, and given the multiple recorded
+# and given that there's multiple, kinda conflicting entries per year, let's
+# find the average for them
+meat_filtered_gbr <- meat_filtered|>
+  group_by(subject, time)|>
+  filter(location == "GBR")|>
+  summarise(year_avg = mean(value))
+  
+
+## Create a line plot which shows the average consumption per year, per animal
+ggplot(meat_filtered_gbr, aes(x = time, y = year_avg, color = subject))+
+  geom_line()+
+  labs(x = "Year", y = "Average Amount Per Year")
